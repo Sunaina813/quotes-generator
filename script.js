ba3cbfcf3c80a63e90fa73ae485f0ef92272542f
Sunaina <sunaina77.sb@gmail.com>
2022-12-27 21:10:46 +0530

    second push

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-const newQuoteBtn = document.getElementById("newQuoteBtn");
-const paraTag = document.querySelector("p");
-const authorname = document.getElementById("authorName");
+const button = document.querySelector("button");
+const paraTag = document.querySelector(".quote");
+const author = document.querySelector(".name");
 let count = 0;
 
-newQuoteBtn.addEventListener("click", fetchUrl);
-
-
-
 async function fetchUrl() {
-  count = count +1;
+  count = count + 1;
   console.log(count);
- 
+
   const apiUrl = "https://type.fit/api/quotes";
   const response = await fetch(apiUrl);
   const data = await response.json();
   console.log(data[count].text);
 
   paraTag.innerText = data[count].text;
-  authorname.innerHTML = data[count].author;
-
-  
+  author.innerHTML = data[count].author;
 }
 fetchUrl();
+button.addEventListener("click", fetchUrl);
